4f407372001bd7f2246487e9d48f3bfb09869679
RabanvdBrand <51989372+RabanvdBrand@users.noreply.github.com>
2025-01-13 16:22:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pandas as pd
 import numpy as np
-import zipfile
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
-from radiomics import featureextractor
+from sklearn.impute import KNNImputer
+#from radiomics import featureextractor
 
-df = pd.read_csv("D:/Virtual studio/MAI3004 test/clinical_info.csv")
+df = pd.read_csv("D:/Virtual studio/MAI3004 test/hecktor2022_clinical_info_training.csv")
+columns_to_drop = ["PatientID", "Tobacco", "Alcohol"]
+df = df.drop(columns_to_drop, axis = 1)
 
 print(df.columns)
 
 print(df.head())
 
-print(df.Tobacco.value_counts())
-df[['Tobacco', "Alcohol", "Surgery","Performance status", "HPV status (0=-, 1=+)"]] = df[['Tobacco', "Alcohol", "Surgery", "Performance status","HPV status (0=-, 1=+)"]].replace("without", 0)
-df[['Tobacco', "Alcohol", "Surgery", "Performance status",  "HPV status (0=-, 1=+)"]] = df[['Tobacco', "Alcohol", "Surgery", "Performance status", "HPV status (0=-, 1=+)"]].astype(int)
-#here 'without' was changed to 0, however we should treat them as missing values
-print(df.Tobacco.value_counts())
-print(df["HPV status (0=-, 1=+)"].value_counts())
+df[["Surgery","Performance status", "HPV status (0=-, 1=+)"]] = df[["Surgery", "Performance status","HPV status (0=-, 1=+)"]].replace("without", np.nan)
+df = df.replace({'Gender' : { 'M' : 0, 'F' : 1}})
+imputer = KNNImputer(n_neighbors=1).set_output(transform = "pandas")
+imputed_data = imputer.fit_transform(df)
+df[["Performance status", "HPV status (0=-, 1=+)"]] = imputed_data[["Performance status", "HPV status (0=-, 1=+)"]]
 
-print(df.Weight.max())
+
+imputed_data_surgery = imputer.fit_transform(df[["Surgery","Performance status", "Chemotherapy", "Age", "Weight"]])
+df["Surgery"] = imputed_data_surgery["Surgery"]
 
 def identify_outliers(df, column_name):
     Q1 = df[column_name].quantile(0.25)
@@ -62,35 +65,25 @@ print(outlier_values)
 
 print(df.Gender.value_counts())
 
-#clinical feature vizualisations
-
-fig, axes = plt.subplots(2, 4, figsize=(18, 10))
+fig, axes = plt.subplots(2, 3, figsize=(18, 10))
 sns.histplot(ax=axes[0,0],data=df, x="Gender", hue="Gender")
 sns.histplot(ax=axes[0,1],data=df, x="Surgery")
-sns.histplot(ax=axes[0,2],data=df, x="Alcohol")
-sns.histplot(ax=axes[0,3],data=df, x="HPV status (0=-, 1=+)")
-sns.histplot(ax=axes[1,0],data=df, x="Chemotherapy")
-sns.histplot(ax=axes[1,1],data=df, x="Weight")
-sns.histplot(ax=axes[1,2],data=df, x="Age")
-sns.histplot(ax=axes[1,3],data=df, x="Performance status")
+sns.histplot(ax=axes[0,2],data=df, x="HPV status (0=-, 1=+)")
+sns.histplot(ax=axes[1,0],data=df, x="Weight")
+sns.histplot(ax=axes[1,1],data=df, x="Age")
+sns.histplot(ax=axes[1,2],data=df, x="Performance status")
 plt.show()
-#sns.boxplot
-
-#sns.histplot(data=df, x="Weight")
-#plt.show()
-
-print(df.isnull().sum())
-print(len(df.PatientID.unique()))
-print(df.shape)
 
 
-print(os.getcwd())
+
+
+#print(os.getcwd())
 #testCase = 'HandNCancer'
 #dataDir = os.path.join(os.getcwd(), "..",  "data")
 
 #imagePath = os.path.join(dataDir, testCase + "imageTR.zip")
 #labelpath = os.path.join(dataDir, testCase + "labelsTR.zip")
-
+"""
 path_img = r"D:\Virtual studio\MAI3004 test\data\imagesTr"
 path_label = r"D:\Virtual studio\MAI3004 test\data\labelsTr"
 store_label_all = r"D:\Virtual studio\MAI3004 test\data\labels_all"
@@ -105,4 +98,7 @@ print(ct_files)
 for img in tqdm(ct_files):
     ct_img_name = os.path.join(path_img, img.replace(".nii.gz", "_0000.nii.gz"))
     result = extractor.execute(path_img + img, path_labels_tum_all + ct_img_name)
+
+"""
+
 
