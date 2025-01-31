637e64e1201bb93b66340888482207e6f9750f2f
RabanvdBrand <51989372+RabanvdBrand@users.noreply.github.com>
2025-01-31 18:14:39 +0100

    Update Label_specifier.py with better comments

--- Label_specifier.py
+++ Label_specifier.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 import os 
 from tqdm import tqdm 
 
-
+# selects the paths to the images and the where the new label images will be stored
 path_img = r"D:\Virtual studio\MAI3004 test\data\imagesTr"
 path_label = r"D:\Virtual studio\MAI3004 test\data\labelsTr"
 store_label_lymph_all = r"D:\Virtual studio\MAI3004 test\data\labels_lymph_all"
 
+#creates a list of all the labels
 img_files = os.listdir(path_label)
 
+#divides the labels based on whether the img label array is equal to either 1 for tumours or 2 for lymph nodes
 for img in tqdm(img_files):
     lab_name = os.path.join(path_label, img)
     lab_img_sitk = sitk.ReadImage(lab_name)
@@ -21,5 +23,5 @@ for img in tqdm(img_files):
     lab_img_arr_tum = lab_img_arr_tum.astype("uint8")
     lab_mod_img = sitk.GetImageFromArray(lab_img_arr_tum)
     lab_mod_img.CopyInformation(lab_img_sitk)
-
+    #saves the new label file in the specified location
     sitk.WriteImage(lab_mod_img, os.path.join(store_label_lymph_all, img))
