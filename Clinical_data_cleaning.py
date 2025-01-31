62ddd709fce54890a1d0793b5255b672ccbb70b7
RabanvdBrand <51989372+RabanvdBrand@users.noreply.github.com>
2025-01-31 18:12:10 +0100

    Update Clinical_data_cleaning.py with better comments

--- Clinical_data_cleaning.py
+++ Clinical_data_cleaning.py
@@ -5,24 +5,29 @@ import matplotlib.pyplot as plt
 import os
 from sklearn.impute import KNNImputer
 
+#load the dataframe
 df = pd.read_csv("D:/Virtual studio/MAI3004 test/all_PET.csv")
+
+#drop the PatientID column
 columns_to_drop = ["PatientID"]
 df = df.drop(columns_to_drop, axis = 1)
 
-print(df.columns)
-
-print(df.head())
-
+#replace the without values with a nan value for imputation
 df[["Surgery","Performance status", "HPV status (0=-, 1=+)"]] = df[["Surgery", "Performance status","HPV status (0=-, 1=+)"]].replace("without", np.nan)
+
+#encode the Gender column with 0 for male and 1 for female
 df = df.replace({'Gender' : { 'M' : 0, 'F' : 1}})
+
+#Impute the performance status and HPV status using K-nearest-neighbours where K is 5
 imputer = KNNImputer(n_neighbors=5).set_output(transform = "pandas")
 imputed_data = imputer.fit_transform(df)
 df[["Performance status", "HPV status (0=-, 1=+)"]] = imputed_data[["Performance status", "HPV status (0=-, 1=+)"]].round().astype(int)
 
-
+#Imput the surgery status using what we thought were appropriate data columns
 imputed_data_surgery = imputer.fit_transform(df[["Surgery","Performance status", "Chemotherapy", "Age", "Weight"]])
 df["Surgery"] = imputed_data_surgery["Surgery"].round().astype(int)
 
+#Finds the outliers in a specific column in the dataframe using the interquartile range
 def identify_outliers(df, column_name):
     Q1 = df[column_name].quantile(0.25)
     Q3 = df[column_name].quantile(0.75)
@@ -36,19 +41,17 @@ def identify_outliers(df, column_name):
     outlier_values = df.loc[outlier_indices, column_name].tolist()
     return outlier_indices, outlier_values
 
+#Identify the outliers for age
 outlier_indices, outlier_values = identify_outliers(df,"Age")
 print(outlier_values)
 print(outlier_indices)
 
+#replace the outliers with nan and then impute them with the mean age
 df.loc[outlier_indices, "Age"] = np.nan
 mean_age = df.Age.mean()
 df.loc[outlier_indices, "Age"] = mean_age
 
-outlier_indices, outlier_values = identify_outliers(df,"Age")
-
-print(outlier_values)
-print(outlier_indices)
-
+# identify the outliers for weight
 outlier_indices, outlier_values = identify_outliers(df,"Weight")
 print(outlier_values)
 print(outlier_indices)
@@ -59,6 +62,5 @@ df.loc[weight_index, "Weight"] = np.nan
 mean_weight = df.Weight.mean()
 df.loc[weight_index, "Weight"] = mean_weight
 
-print(df.head())
-
+#save the cleaned df to csv
 df.to_csv('All_PET.csv')
